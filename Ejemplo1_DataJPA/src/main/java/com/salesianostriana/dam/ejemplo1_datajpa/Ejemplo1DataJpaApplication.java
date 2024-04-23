a2b67215d5a317d46086938d79e3bbd1d6f7cd29
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2024-04-23 12:32:09 +0200

    Ejemplo_1 Data JPA añadido

---
+++ Ejemplo1_DataJPA/src/main/java/com/salesianostriana/dam/ejemplo1_datajpa/Ejemplo1DataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejemplo1_datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejemplo1DataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejemplo1DataJpaApplication.class, args);
+	}
+
+}
---
+++ Ejemplo1_DataJPA/src/test/java/com/salesianostriana/dam/ejemplo1_datajpa/Ejemplo1DataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejemplo1_datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejemplo1DataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
