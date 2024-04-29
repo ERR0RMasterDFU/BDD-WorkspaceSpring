1f42812ea3b6356dedb65d8997441fb89977bc15
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2024-04-29 10:08:40 +0200

    Ejemplo 4 añadido

---
+++ ServicioBase/src/main/java/com/salesianostriana/dam/serviciobase/Apuntes.java
@@ -0,0 +1,16 @@
+package com.salesianostriana.dam.serviciobase;
+
+public class Apuntes {
+
+	
+	/* Busquen un objeto por su identificador (si no OPTIONAL).
+	 * Para borrar.
+	 * Para devolver todos los objetos.
+	 
+	
+	
+	
+	
+	ALTER SEQUENCE alumno_seq RESTART WITH 101; HACE QUE DESPUÉS DE INSERTAR LOS DATOS SE QUEDEN EN ES NÚMERO Y SIGA SUMANDO SI SE INSERTA
+	2ªFORMA: ID como SQL SENTENCIA EN MOCAROO Y ESCRIBIR alumno_seq;*/
+}
---
+++ ServicioBase/src/main/java/com/salesianostriana/dam/serviciobase/service/AlumnoServicio.java
@@ -0,0 +1,14 @@
+package com.salesianostriana.dam.serviciobase.service;
+
+import org.springframework.stereotype.Service;
+
+import com.salesianostriana.dam.serviciobase.BaseServiceImp;
+import com.salesianostriana.dam.serviciobase.model.Alumno;
+import com.salesianostriana.dam.serviciobase.repos.AlumnoRepositorio;
+
+@Service
+public class AlumnoServicio extends BaseServiceImp<Alumno, Long, AlumnoRepositorio>{
+			
+		
+	}
+
